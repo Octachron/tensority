2a5fe429498d301226129690f910504ae341dea4
octachron <octa@polychoron.fr>
2016-04-09 21:12:48 +0200

    Use fortran layout in small_matrix

--- src/small_matrix.ml
+++ src/small_matrix.ml
@@ -3,6 +3,10 @@ module A = Array
 open Signatures
 let delta = Tensority_misc.delta
 
+let (@?) a n = A.unsafe_get a n
+let ( % ) a n x = A.unsafe_set a n x
+let ( =: ) = (@@)
+
 type 'a t = {lines: 'b H.t; array:float array}
   constraint 'a = 'b * 'c
 
@@ -19,8 +23,8 @@ let init lines (rows:' b H.t) f : ('a * 'b) t =
   let nl = H.to_int lines and nr = H.to_int rows in
   let array = A.make_float (nl * nr ) in
   let pos = ref 0 in
-  for i = 0 to nl - 1 do
-    for j = 0 to nr - 1 do
+  for j = 0 to nr - 1 do
+    for i = 0 to nl - 1 do
       array.(!pos) <- f i j; incr pos
     done;
   done;
@@ -29,10 +33,10 @@ let init lines (rows:' b H.t) f : ('a * 'b) t =
 let square dim f = f dim dim
 
 let get (mat:('a * 'b) t) (i:'a H.t) (j:'b H.t)=
-  Array.unsafe_get mat.array (H.to_int i * H.to_int mat.lines + H.to_int j)
+  Array.unsafe_get mat.array (H.to_int j * H.to_int mat.lines + H.to_int i)
 
 let set (mat:('a * 'b) t) (i:'a H.t) (j:'b H.t) x =
-  Array.unsafe_set mat.array (H.to_int i * H.to_int mat.lines + H.to_int j) x
+  Array.unsafe_set mat.array (H.to_int j * H.to_int mat.lines + H.to_int i) x
 
 let dims mat = let l = H.to_int mat.lines in
   l, Array.length mat.array / l
@@ -46,13 +50,13 @@ let size mat = Array.length mat.array
 let map f m = { m with array = A.map f m.array }
 
 let map2 ( <@> ) (m:'a t) (n:'a t): 'a t =
-  let array = Array.mapi (fun i x -> x <@> A.unsafe_get n.array i) m.array in
+  let array = Array.mapi (fun i x -> x <@> n.array @? i) m.array in
   { m with array }
 
 let fold_2 f acc (m:' a t) (n:'a t) =
   let acc = ref acc in
   for i = 0 to size m - 1 do
-    acc := f !acc (A.unsafe_get m.array i) (A.unsafe_get n.array i)
+    acc := f !acc (m.array @? i) (n.array @? i)
   done;
   !acc
 
@@ -69,7 +73,7 @@ let diag v =
   let dim = Small_vec.typed_dim v in
   let n = H.to_int dim in
   let a = Array.make (n * n) 0. in
-  H.iter_on dim ( fun k -> A.unsafe_set a (H.to_int k * n) @@ Small_vec.get v k );
+  H.iter_on dim ( fun k -> a % (H.to_int k * n) =: (Small_vec.get v k) );
   create dim dim a
 
 let id dim = square dim init delta
@@ -79,12 +83,12 @@ let transpose (mat:('a *' b) t) : ('b * 'a ) t =
   let l, r = dims mat in
   let lines = H.create @@ r in
   let dir = ref 0 and tr = ref 0 in
-  for i = 0 to l - 1 do
-    tr := i;
-    for j = 0 to r - 1 do
-      A.unsafe_set array !tr @@ A.unsafe_get array !dir;
+  for j = 0 to r - 1 do
+    tr := j;
+    for i = 0 to l - 1 do
+      array % !tr @@ array @? !dir;
       incr dir;
-      tr := !tr + r
+      tr := !tr + l
     done;
   done;
   { lines ; array }
@@ -92,28 +96,32 @@ let transpose (mat:('a *' b) t) : ('b * 'a ) t =
 
 module Operators = struct
   module Matrix_specific = struct
-    let (@) (m: ('a * 'b) t) (v:'b Small_vec.t) : 'a Small_vec.t =
-      Small_vec.init m.lines (fun i ->
-          let sum = ref 0. in
-          let off =  i* H.to_int m.lines in
-          H.iter_on (Small_vec.typed_dim v) (fun k ->
-              sum := !sum +.
-                     A.unsafe_get m.array (off + H.to_int k ) *. Small_vec.get v k
-            );
-          !sum
-        )
 
+    (** matrix application: fortran layout *)
+    let (@) (m: ('a * 'b) t) (v:'b Small_vec.t) : 'a Small_vec.t =
+      let l = H.to_int m.lines in
+      let array = Array.make l 0. in
+      let pos = ref 0 in
+      H.iter_on (Small_vec.typed_dim v) (fun k ->
+        for i = 0 to l - 1 do
+          array % i =: (array @? i) +. m.array.(!pos) *. Small_vec.(v.(k));
+          incr pos
+        done
+        )
+      ; Small_vec.unsafe_create m.lines array
+
+    (** matrix multiplication: fortran layout *)
     let ( * ) (m: ('a * 'b) t) (n: ('b * 'c) t): ('a * 'c) t =
       let l, c = typed_dims n in
       init m.lines c (fun i j ->
           let sum = ref 0. in
-          let off = i * H.to_int m.lines in
-          let off_n = ref j in
+          let off = j * H.to_int m.lines in
+          let off_n = ref i in
           let n_k = H.to_int l in
           for k = 0 to n_k - 1 do
             sum := !sum +.
-                   A.unsafe_get m.array (off + k)
-                   *. A.unsafe_get n.array (!off_n);
+                   (m.array @? !off_n )
+                   *. ( n.array @? off + k);
             off_n := !off_n + n_k
           done;
           !sum
