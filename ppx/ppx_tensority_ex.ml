663d24a57849818d96f5f3db9bf94e644b3c4e4c
octachron <octa@polychoron.fr>
2016-05-16 14:07:26 +0200

    Ppx reorganisation

--- ppx/ppx_tensority_ex.ml
+++ ppx/ppx_tensority_ex.ml
@@ -27,151 +27,180 @@ module Lid = struct
   let ( $ ) f x = Lapply(f,x)
 end
 
-type row_tag = {
-  label: label
-; attributes: attributes
-; empty_type: bool
-; conjunction: core_type list
-}
-
-let tag  ?(empty_type=false) ?(conjunction=[]) ?(attributes=[]) label =
-  Rtag (label, attributes, empty_type, conjunction )
-
-let set typ = Rinherit typ
-
-type simple_var = core_type list
-
-let var_simple loc ?(closed=Closed) ?lower_bound_opt upper_bound =
-  {
-    ptyp_desc = Ptyp_variant (upper_bound, closed, lower_bound_opt)
-  ; ptyp_loc = loc
-  ; ptyp_attributes = []
+module Polyvar = struct
+  type row_tag = {
+    label: label
+  ; attributes: attributes
+  ; empty_type: bool
+  ; conjunction: core_type list
   }
 
+  let tag  ?(empty_type=false) ?(conjunction=[]) ?(attributes=[]) label =
+    Rtag (label, attributes, empty_type, conjunction )
 
-let var loc types =
+  let set typ = Rinherit typ
+
+  type simple_var = core_type list
+
+  let var_simple loc ?(closed=Closed) ?lower_bound_opt upper_bound =
+    {
+      ptyp_desc = Ptyp_variant (upper_bound, closed, lower_bound_opt)
+    ; ptyp_loc = loc
+    ; ptyp_attributes = []
+    }
+
+
+  let var loc types =
     if List.length types > 1 then
       var_simple loc ~closed:Closed ~lower_bound_opt:[] types
     else
       var_simple loc ~closed:Closed types
 
-let var_low types =
-  var_simple ~closed:Closed ~lower_bound_opt:[] types
+  let var_low types =
+    var_simple ~closed:Closed ~lower_bound_opt:[] types
 (*
 let var ?(conjunction=[]) label = variant @@ tag ~conjunction label
 *)
+end
 
-let label_of_int n = "_" ^ string_of_int n
 
-let t = tag ~empty_type:true "T"
+module Ints = struct
+
+
+let to_label n = "_" ^ string_of_int n
+
+let t = Polyvar.tag ~empty_type:true "T"
 let eq = [%type: eqm]
 let lt = [%type: ltm]
 
-let nat_expr loc kind typ value =
-  [%expr let open Nat in let nat: ([%t typ],[%t kind]) Nat.t = create [%e value] in nat ]
-  [@@metaloc loc]
+module Expr = struct
+  let nat loc kind typ value =
+    [%expr let open Nat in let nat: ([%t typ],[%t kind]) Nat.t =
+                             create [%e value] in nat ][@metaloc loc]
 
-let shape_expr loc kind typ value =
-  [%expr let nat = [%e nat_expr loc kind typ value] in
-         Elt (nat)
-  ][@@metaloc loc]
+  let shape loc kind typ value =
+    [%expr let nat = [%e nat loc kind typ value] in
+           Elt (nat)
+    ][@@metaloc loc]
 
-let int_expr loc n = H.Exp.constant ~loc (H.Const.int n)
+  let int loc n = H.Exp.constant ~loc (H.Const.int n)
+end
 
-let digit k t = tag ~conjunction:[t] (label_of_int k)
+  (* a digit [k] followed by [t] *)
+  let digit k t = Polyvar.tag ~conjunction:[t] (to_label k)
+
+  (* number of digits *)
+  let size n = int_of_float @@ log10 @@ float n
 
 
-let size n = int_of_float @@ log10 @@ float n
-
-
-let rec digit_split n =
+  let rec digit_split n =
   if n < 10 then n, 0
   else
     let d, k = digit_split (n/10) in
     d, k * 10 + n mod 10
 
-let rec type_size_int_rec loc n inner =
-  if n < 10 then
-    var loc [ digit n inner ]
-  else
-    let l, k = n mod 10, n / 10 in
-    type_size_int_rec loc k @@ var loc [ digit l inner ]
+  module Eq = struct
+    (** '( =n ) nat *)
+    module Type = struct
+      let rec int_rec loc n inner =
+        let open Polyvar in
+        if n < 10 then
+          var loc [ digit n inner ]
+        else
+          let l, k = n mod 10, n / 10 in
+          int_rec loc k @@ var loc [ digit l inner ]
 
-let type_size_int loc n = type_size_int_rec loc n @@ var loc [t]
+      let int loc n = int_rec loc n @@ Polyvar.var loc [t]
+    end
 
-let eq_int loc n =
-  shape_expr loc eq (type_size_int loc n) (int_expr loc n)
+    let int loc n =
+      Expr.shape loc eq (Type.int loc n) (Expr.int loc n)
 
-let eq_nat loc n =
-  nat_expr loc eq (type_size_int loc n) (int_expr loc n)
+    let nat loc n =
+      Expr.nat loc eq (Type.int loc n) (Expr.int loc n)
+  end
 
+  module Types = struct
 
-let ($) f ts = T.constr f ts
+    let ($) f t  = T.constr f t
 
-let gtp loc k t =
-  let lid = Lid.( !"Gtp" <*> label_of_int k ) in
-  let lid = mkloc loc lid in
-  (lid $ [t])
+    let gtp loc k t =
+      let lid = Lid.( !"Gtp" <*> to_label k ) in
+      let lid = mkloc ~loc lid in
+      (lid $ [t])
 
-let lep loc k t =
-  let lid = Lid.( !"Lep" <*> label_of_int k ) in
-  let lid = mkloc loc lid in
-  (lid $ [t])
+    let lep loc k t =
+      let lid = Lid.( !"Lep" <*> to_label k ) in
+      let lid = mkloc ~loc lid in
+      (lid $ [t])
 
-let all loc t = gtp loc 0 t
-let ending loc = var loc [t; set @@ all loc @@ T.any () ]
+    let all loc t = gtp loc 0 t
+    let ending loc =
+      let open Polyvar in
+      var loc [t; set @@ all loc @@ T.any () ]
 
-let rec digits loc k = if k = 0 then
-    ending loc
-  else
-   var_low loc [ set @@ all loc (digits loc @@ k - 1) ]
-
-let rec type_lt_int_aux loc k len inner =
-  let inner d =
-    let l = [ digit d inner ] in
-    let l =  if d<9 then
-        (set @@ gtp loc (d+1) @@ digits loc len ) :: l
+    let rec digits loc k = if k = 0 then
+        ending loc
       else
-        l
-    in
-    if d > 0 then
-      var loc @@ set (lep loc (d-1) @@ digits loc @@ 1 + len) :: l
-    else
-      var_low loc l
-  in
-  if k < 10 then
-      inner k
-  else
-    let d, k = k mod 10, k / 10 in
-    type_lt_int_aux loc k (len + 1) (inner d)
+        let open Polyvar in
+        var_low loc [ set @@ all loc (digits loc @@ k - 1) ]
+  end
+  module Lt = struct
 
-let type_lt_int loc k =
-  if k = 0 then
-    ending loc
-  else
-    type_lt_int_aux loc k 0 (var_low loc [ set @@ all loc @@ ending loc ] )
+    module Type = struct
+      open Types
+      let rec int_aux loc k len inner =
+        let inner d =
+          let open Polyvar in
+          let l = [ digit d inner ] in
+          let l =  if d<9 then
+              (set @@ gtp loc (d+1) @@ digits loc len ) :: l
+            else
+              l
+          in
+          if d > 0 then
+            var loc @@ set (lep loc (d-1) @@ digits loc @@ 1 + len) :: l
+          else
+            var_low loc l
+        in
+        if k < 10 then
+          inner k
+        else
+          let d, k = k mod 10, k / 10 in
+          int_aux loc k (len + 1) (inner d)
 
-let lt_int loc k =
-  shape_expr loc lt (type_lt_int loc k) (int_expr loc k)
+      let int loc k =
+        if k = 0 then
+          ending loc
+        else
+          let open Polyvar in
+          int_aux loc k 0 (var_low loc [ set @@ all loc @@ ending loc ] )
+    end
 
-let lt_nat loc k =
-  nat_expr loc lt (type_lt_int loc k) (int_expr loc k)
+    let int loc k =
+      Expr.shape loc lt (Type.int loc k) (Expr.int loc k)
 
-let expect_int name = function
-  | { pexp_desc = Pexp_constant Pconst_integer(n, None) } ->
+    let nat loc k =
+      Expr.nat loc lt (Type.int loc k) (Expr.int loc k)
+  end
+end
+
+let expect_int name = function[@warning "-4"]
+  | { pexp_desc = Pexp_constant Pconst_integer(n, None); _  } ->
     int_of_string n
   | e -> error e.pexp_loc "[%%%s] expected an integer as first argument"
            name
 
-let constant loc super =function
+let constant loc super =function[@warning "-4"]
   | Pconst_integer (n,Some m ) ->
     begin
       let n = int_of_string n in
+      let open Ints in
       match m with
-      | 'k' -> eq_nat loc n
-      | 's' -> eq_int loc n
-      | 'i' -> lt_int loc n
-      | 'j' -> lt_nat loc n
+      | 'k' -> Eq.nat loc n
+      | 's' -> Eq.int loc n
+      | 'i' -> Lt.int loc n
+      | 'j' -> Lt.nat loc n
       | _ -> super
     end
   | _ ->  super
@@ -181,35 +210,45 @@ let rec sequence loc = function
   | a :: q -> H.Exp.sequence ~loc a (sequence loc q)
   | _ -> assert false
 
-let rec extract_sequence = function
+let rec extract_sequence = function[@warning "-4"]
   | [%expr [%e? h]; [%e? r] ] -> h :: (extract_sequence r)
   | e -> [e]
 
-let rec to_list loc = function
+let rec to_list loc = function[@warning "-4"]
   | [] -> [%expr [] ][@metaloc loc]
   | [e] -> [%expr [[%e e]] ][@metaloc loc]
   | a::q -> [%expr [%e a]::[%e to_list loc q] ][@metaloc loc]
 
-let rec rewrite_tuples kont = function
+let rewrite_tuples kont = function[@warning "-4"]
   | {pexp_desc = Pexp_tuple l; _ } as e ->
     to_list e.pexp_loc @@ List.map kont l
   | e -> to_list e.pexp_loc [ kont e ]
 
-let rec rewrite_seq kont seq =
+let rewrite_seq kont seq =
   let l = List.map kont @@ extract_sequence seq in
   match l with
   | [a] -> a
   | l -> H.Exp.tuple ~loc:seq.pexp_loc l
 
-let index_rewriter mapper = rewrite_seq @@ rewrite_tuples mapper
+let simple_index_rewriter mapper = rewrite_seq @@ rewrite_tuples mapper
+let index_rewriter mapper =
+  let open Ast_mapper in
+  let map = mapper.expr mapper in
+  function[@warning "-4"]
+      | [%expr [%e? i ]; __ ] ->
+        [%expr [%e rewrite_tuples map i], []][@metaloc i.pexp_loc]
+      | [%expr __ ; [%e? i ] ] ->
+        [%expr [], [%e rewrite_tuples map i]][@metaloc i.pexp_loc]
+      | e -> simple_index_rewriter map e
 
 
-let vec loc = function
+
+let vec loc = function[@warning "-4"]
   | { pexp_desc = Pexp_tuple s; _ } as e ->
     let a = {e with pexp_desc = Pexp_array s} in
-    let nat = eq_int loc @@ List.length s in
+    let nat = Ints.Eq.int loc @@ List.length s in
   [%expr unsafe_create Shape.[[%e nat]] [%e a]][@metaloc loc]
-  | e ->
+  | _ ->
     error loc "expected tuple in [%%vec ...]"
 
 type 'a loc = 'a Location.loc
@@ -218,7 +257,7 @@ type 'a nested_list =
   | Elt of 'a
   | Nested of 'a nested_list loc * 'a nested_list loc list
 
-let rec extract_nested ({name} as k) loc level e =
+let rec extract_nested ({name; _ } as k) loc level e =
   if level = 0 then
     mkloc ~loc @@ Elt e
   else
@@ -240,6 +279,7 @@ let rec extract_nested ({name} as k) loc level e =
     in
     let extr e =  extract_nested k loc (level - 1) e  in
     mkloc ~loc @@ Nested( extr a, List.map extr q)
+  [@@warning "-4"]
 
 let apply_loc f x = Location.( (f x.txt).txt )
 
@@ -247,8 +287,8 @@ let rec compute_and_check_shape kind loc level =
   let error_ppx = error in
   let open Location in
   function
-  | { txt = Elt _ } -> []
-  | { txt = Nested (a,q) } ->
+  | { txt = Elt _ ; _ } -> []
+  | { txt = Nested (a,q); _ } ->
     let n = 1 + List.length q in
     let shape0 =
       compute_and_check_shape kind a.loc (level - 1) a in
@@ -273,7 +313,7 @@ let create_array loc level e =
   let nested = extract_nested kind loc level e in
   let shape_int = compute_and_check_shape kind loc level nested in
   let l = flatten_nested nested [] in
-  let shape = to_list loc @@ List.map (eq_int loc) shape_int in
+  let shape = to_list loc @@ List.map (Ints.Eq.int loc) shape_int in
   let array = H.Exp.array ~loc l in
   [%expr [%e kind.fn] [%e shape] [%e array] ]
 
@@ -282,7 +322,7 @@ let rec split n l =
     match l with
     | a :: q ->
       let left, right = split (n-1) q in
-      a :: left, q
+      a :: left, right
     | [] -> raise (Invalid_argument (Printf.sprintf "split %d [] is not valid" n))
 
 let create_tensor loc ~contr ~cov e =
@@ -292,7 +332,7 @@ let create_tensor loc ~contr ~cov e =
   let shape_int = compute_and_check_shape kind loc level nested in
   let l = flatten_nested nested [] in
   let contr, cov  = split contr shape_int in
-  let shape l = to_list loc @@ List.map (eq_int loc) l in
+  let shape l = to_list loc @@ List.map (Ints.Eq.int loc) l in
   let array = H.Exp.array ~loc l in
   [%expr [%e kind.fn] ~contr:[%e shape contr] ~cov:[%e shape cov] [%e array] ]
 
@@ -310,47 +350,78 @@ let range loc ?by start stop =
     error loc "[%%range]: invalid argument step %d ≤ 0"
       step
   ; let len = 1 + (stop - start) / step in
-    let start = lt_nat loc start and stop = lt_nat loc stop
-    and len = eq_nat loc len and step = int_expr loc step in
+    let start = Ints.Lt.nat loc start and stop = Ints.Lt.nat loc stop
+    and len = Ints.Eq.nat loc len and step = Ints.Expr.int loc step in
     [%expr Shape.Range(
            Shape.Range.create
              ~start:[%e start] ~stop:[%e stop] ~step:[%e step] ~len:[%e len]
          )
     ][@metaloc loc]
 
-let expr mapper = function
+let index_access kont mapper =
+  let map_std = default.expr mapper in
+  let map a i = map_std a, index_rewriter mapper i in
+  function[@warning "-4"]
+  | [%expr [%e? a].[ [%e? i] ] ] as e ->
+    let a, i = map a i in
+    [%expr [%e a].[ [%e i]] ][@metaloc e.pexp_loc]
+  | [%expr [%e? a].( [%e? i] ) ] as e ->
+    let a, i = map a i in
+    [%expr [%e a].( [%e i] ) ][@metaloc e.pexp_loc]
+  | e -> kont mapper e
+
+let index_assign kont mapper =
+  let map_std a = default.expr mapper a in
+  let map a i v = map_std a, index_rewriter mapper i, map_std v in
+  function[@warning "-4"]
+  | [%expr [%e? a].[[%e? i] ] <- [%e? v] ] as e ->
+    let a,i, v = map a i v in
+    [%expr [%e a].[[%e i]]<- [%e v] ][@metaloc e.pexp_loc]
+  | [%expr [%e? a].( [%e? i] ) <- [%e? v] ] as e ->
+    let a,i, v = map a i v in
+    [%expr [%e a].[[%e i]]<- [%e v] ][@metaloc e.pexp_loc]
+  | e -> kont mapper e
+
+let const_mapper kont mapper = function[@warning "-4"]
   | {pexp_desc = Pexp_constant c;  _ } as e ->
     constant e.pexp_loc e c
-  | [%expr [%e? a].[[%e? i]]] as e ->
-    let a = default.expr mapper a in
-    let i = index_rewriter (mapper.expr mapper) i in
-    [%expr [%e a].[[%e i]]][@metaloc e.pexp_loc]
-  | [%expr [%e? a].[[%e? i]] <- [%e? v] ] as e ->
-    let a = default.expr mapper a in
-    let i = index_rewriter (mapper.expr mapper) i in
-    let v = default.expr mapper v in
-    [%expr [%e a].[[%e i]]<- [%e v] ][@metaloc e.pexp_loc]
+  | e -> kont mapper e
+
+let array_mapper kont mapper = function[@warning "-4"]
   | [%expr [%array [%e? n] [%e? array] ] ] as e ->
     create_array e.pexp_loc (expect_int ma.name n) array
   | [%expr [%array [%e? array] ] ] as e ->
     create_array e.pexp_loc 1 array
+  | e -> kont mapper e
+
+let tensor_mapper kont mapper=
+function[@warning "-4"]
   | [%expr [%tensor [%e? contr] [%e? cov] [%e? array] ] ] as e ->
     create_tensor e.pexp_loc
       ~contr:(expect_int tensor.name contr)
       ~cov:(expect_int tensor.name cov)
       array
-  | [%expr [%tensor [%e? array] ] ] as e ->
+  | [%expr [%vec [%e? array] ] ] as e ->
     create_tensor e.pexp_loc ~contr:1 ~cov:0 array
-  | [%expr [%range [%e? start] -- [%e? stop] ~by:[%e? step] ] ] as e
+  | [%expr [%matrix [%e? array] ] ] as e ->
+    create_tensor e.pexp_loc ~contr:1 ~cov:1 array
+  | e -> kont mapper e
+
+let range_mapper kont mapper = function[@warning "-4"]
+  | [%expr [%range [%e? start] [%e? stop] ~by:[%e? step] ] ] as e
   | ( [%expr [%e? start] #-># [%e? stop] #/# [%e? step] ] as e ) ->
     range e.pexp_loc ~by:step start stop
-  | [%expr [%range [%e? start] -- [%e? stop] ] ] as e
+  | [%expr [%range [%e? start] [%e? stop] ] ] as e
   | ([%expr [%e? start] #-># [%e? stop] ] as e) ->
     range e.pexp_loc start stop
-  | [%expr [%vec [%e? t] ] ] as e ->
-    vec e.pexp_loc t
-  | e -> default.expr mapper e
+  | e -> kont mapper e
 
-let mapper arg = Ast_mapper.{ default_mapper with expr }
+let expr mapper =
+  let map =
+  range_mapper @@ tensor_mapper @@ array_mapper @@ const_mapper @@
+  index_access @@ index_assign @@ default.expr in
+  map mapper
+
+let mapper _arg = Ast_mapper.{ default_mapper with expr }
 
 let () = Ast_mapper.register "ppx_tensority" mapper
