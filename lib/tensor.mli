ab1877975335d8e44db5e914d4ae17f56bae5fcb
octachron <octa@polychoron.fr>
2016-08-01 14:08:30 +0200

    tensor documentation

--- lib/tensor.mli
+++ lib/tensor.mli
@@ -1,82 +1,178 @@
-module MA = Multidim_array
+(** Compared to multidimensional arrays, tensors carry supplementary
+    information to facilitate common linear algebra information.
 
+    More precisely, the dimensions of a tensor are divided between covariant
+    and contravariant dimensions. A vector is for instance a (1,0) tensor,
+    and a matrix is an (1,1) tensor. Matrix multiplication and matrix - vector
+    multiplication is then a special case of tensor contraction:
+    A (full) tensor contraction of a (i,k) tensor with a (k,j) tensor yields
+    a (i,j) tensors. Matrix multiplication corresponds then to the case
+    (1,1) tensor * (1,1) tensor → (1,1) tensor and matrix-vector multiplication
+    is the case (1,1) tensor * (1,0) tensor → (1,0) tensor.
+
+    Note that this module implements for now an euclidian geometry, so the
+    distinction between contravariant and covariant indices is mainly formal.
+
+*)
+
+(** {2 Type definitions }*)
+
+(** [<contr: dims ; cov:dims' > t] encodes a tensor
+with contravariant dimensions [dims] and covariant dimension [dims']  *)
 type 'c t constraint 'c = < contr : 'a; cov : 'b >
+
+
+(** Shortcut type for vectors = (1,0) tensors *)
 type 'dim vec = < contr : 'dim Shape.single; cov : Shape.empty > t
+
+(** Shortcut type for matrices = (1,1) tensors *)
 type ('l, 'c) matrix = < contr : 'l Shape.single; cov : 'c Shape.single > t
+
+(** Shortcut type for the (2,1)-tensors *)
 type ('d1, 'd2, 'd3) t3 =
   < contr : ('d1, 'd2) Shape.pair; cov : 'd3 Shape.single > t
 
-module Unsafe : sig
-val create :
-  contr:'a Shape.eq ->
-  cov:'b Shape.eq -> float array -> < contr : 'a; cov : 'b > t
-end
+(** {2 Printing} *)
+(** Pretty-printer for tensor *)
+val pp : Format.formatter -> < contr : 'a; cov : 'b > t -> unit
 
+(** Conversion to string *)
+val show : < contr : 'a; cov : 'b > t -> string
+
+(** {2 Access operators} *)
+(** tensor access:
+    with ppx_tensority: [ t.(i_1, ..., i_n; j_1, ..., j_n) ]
+    without ppx [ t.([i_1;...;i_n], [j_1;...,j_n]) ]
+*)
 val get: < contr : 'a; cov : 'b > t -> 'a Shape.lt * 'b Shape.lt -> float
   [@@indexop.arraylike]
 
+(** tensor access:
+    with ppx_tensority: [ t.(i_1, ..., i_n; j_1, ..., j_n) <- x ]
+    without ppx [ t.([i_1;...;i_n], [j_1;...,j_n]) <- x ]
+*)
 val set: < contr : 'a; cov : 'b > t -> 'a Shape.lt * 'b Shape.lt -> float -> unit
   [@@indexop.arraylike]
 
-val cov_size : < contr : 'a; cov : 'b > t -> int
-val contr_size : < contr : 'a; cov : 'b > t -> int
-val len : < contr : 'a; cov : 'b > t -> int
-val contr_dims : < contr : 'a; cov : 'b > t -> 'a Shape.eq
-val cov_dims : < contr : 'a; cov : 'b > t -> 'b Shape.eq
-val is_sparse : < contr : 'a; cov : 'b > t -> bool
+(** {3 Specialized access operator for vector, matrix and standard 3-tensor} *)
 
-
-val const :
-  contr:'a Shape.eq -> cov:'b Shape.eq -> float -> < contr : 'a; cov : 'b > t
-
-val zero : contr:'a Shape.eq -> cov:'b Shape.eq -> < contr : 'a; cov : 'b > t
-
-val init_sh :
-  ('a Shape.lt -> 'b Shape.lt -> float) ->
-  contr:'a Shape.eq -> cov:'b Shape.eq -> < contr : 'a; cov : 'b > t
-
-val pp : Format.formatter -> < contr : 'a; cov : 'b > t -> unit
-val show : < contr : 'a; cov : 'b > t -> string
-
-val reshape :
-  < contr : 'a; cov : 'b > t ->
-  'c Shape.eq * 'd Shape.eq -> < contr : 'c; cov : 'd > t
-
-val matrix :
-  'a Nat.eq -> 'b Nat.eq -> ('a Nat.lt -> 'b Nat.lt -> float) -> ('a, 'b) matrix
-
-val sq_matrix :
-  'a Nat.eq -> ('a Nat.lt -> 'a Nat.lt -> float) -> ('a, 'a) matrix
-
-val vector : 'a Nat.eq -> ('a Nat.lt -> float) -> 'a vec
-val vec_dim : 'dim vec -> 'dim Nat.eq
-val set_3 :
-  ('a, 'b, 'c) t3 -> 'a Nat.lt -> 'b Nat.lt -> 'c Nat.lt -> float -> unit
-  [@@indexop]
-
-val get_3 :
-  ('a, 'b, 'c) t3 -> 'a Nat.lt -> 'b Nat.lt -> 'c Nat.lt -> float
-  [@@indexop]
-
-val set_2 : ('a, 'b) matrix -> 'a Nat.lt -> 'b Nat.lt -> float -> unit
+val get : 'a vec -> 'a Nat.lt -> float
   [@@indexop]
 
 val get_2 : ('a, 'b) matrix -> 'a Nat.lt -> 'b Nat.lt -> float
   [@@indexop]
 
+
+val get_3 :
+  ('a, 'b, 'c) t3 -> 'a Nat.lt -> 'b Nat.lt -> 'c Nat.lt -> float
+  [@@indexop]
+
 val set : 'a vec -> 'a Nat.lt -> float -> unit
   [@@indexop]
 
-val get : 'a vec -> 'a Nat.lt -> float
+val set_2 : ('a, 'b) matrix -> 'a Nat.lt -> 'b Nat.lt -> float -> unit
   [@@indexop]
 
-val delta : ('a, 'b) Nat.t -> ('c, 'd) Nat.t -> float
-val id : 'a Nat.eq -> ('a, 'a) matrix
-val base : 'a Nat.eq -> 'a Nat.lt -> 'a vec
+val set_3 :
+  ('a, 'b, 'c) t3 -> 'a Nat.lt -> 'b Nat.lt -> 'c Nat.lt -> float -> unit
+  [@@indexop]
+
+
+(** {2 Shape functions} *)
+(** total size of the covariant dimensions *)
+val cov_size : < contr : 'a; cov : 'b > t -> int
+
+(** total size of the contravariant dimensions *)
+val contr_size : < contr : 'a; cov : 'b > t -> int
+
+(** total size of the tensor *)
+val len : < contr : 'a; cov : 'b > t -> int
+
+(** Contravariant shape *)
+val contr_dims : < contr : 'a; cov : 'b > t -> 'a Shape.eq
+
+(** Covariant shape *)
+val cov_dims : < contr : 'a; cov : 'b > t -> 'b Shape.eq
+
+(** Dimension of an endomorphism *)
 val endo_dim : ('a, 'a) matrix -> 'a Nat.eq
 
+(** A tensor is sparse if the number of elements within
+    addressed by the tensor is strictly less than
+    the lenght of the underlying array *)
+val is_sparse : < contr : 'a; cov : 'b > t -> bool
+
+(** {2 Construction function } *)
+
+(** Unsafe module for function with a runtime check *)
+module Unsafe : sig
+
+  (** Create a tensor from an array.
+      @raise Dimension_error if the array lenght is not compatible
+      with the given dimension*)
+  val create :
+    contr:'a Shape.eq ->
+    cov:'b Shape.eq -> float array -> < contr : 'a; cov : 'b > t
+end
+
+(** Creates a tensor with constant coefficients *)
+val const :
+  contr:'a Shape.eq -> cov:'b Shape.eq -> float -> < contr : 'a; cov : 'b > t
+
+(** zero tensor *)
+val zero : contr:'a Shape.eq -> cov:'b Shape.eq -> < contr : 'a; cov : 'b > t
+
+(** [init_sh f sh1 sh2] creates a tensor [ t ] such
+    that for all s≺sh1 and s2≺sh2 [ t.(s,s2) = f s s2
+*)
+val init_sh :
+  ('a Shape.lt -> 'b Shape.lt -> float) ->
+  contr:'a Shape.eq -> cov:'b Shape.eq -> < contr : 'a; cov : 'b > t
+
+(** [vector n f] computes the n vector [ v_{i} = f i } *)
+val vector : 'a Nat.eq -> ('a Nat.lt -> float) -> 'a vec
+
+(** [matrix k l f] computes the k×l matrix [ m_{i,j} = f i j } *)
+val matrix :
+  'a Nat.eq -> 'b Nat.eq -> ('a Nat.lt -> 'b Nat.lt -> float) -> ('a, 'b) matrix
+
+(** [sq_matrix n f] computes the square matrix [matrix n n f] *)
+val sq_matrix :
+  'a Nat.eq -> ('a Nat.lt -> 'a Nat.lt -> float) -> ('a, 'a) matrix
+
+(** copy a tensor *)
+val copy : < contr : 'a; cov : 'b > t -> < contr : 'a; cov : 'b > t
+
+(** {2 Reshaping and indice manipulation *)
+val reshape :
+  < contr : 'a; cov : 'b > t ->
+  'c Shape.eq * 'd Shape.eq -> < contr : 'c; cov : 'd > t
+
+(** [transpose t] is the totally transposed tensor such that
+    (transpose t).(s;s') = t.(s';s).
+    Note that this transposition supposes that space is flat to be geometrically
+    valid.
+*)
 val transpose : < contr : 'a; cov : 'b > t -> < contr : 'b; cov : 'a > t
 
+
+(** {2 Linear basis function} *)
+
+(** [delta i j = 1. ] if [i = j], [0] otherwise *)
+val delta : ('a, 'b) Nat.t -> ('c, 'd) Nat.t -> float
+
+(** [id n] is the identity matrix in dimension n *)
+val id : 'a Nat.eq -> ('a, 'a) matrix
+
+(** [base n i] is the n vector such that [ v.{j} = delta i j ] *)
+val base : 'a Nat.eq -> 'a Nat.lt -> 'a vec
+
+(** {2 Tensor level operation} *)
+
+(** Tensor contraction (or multiplication) of a (d,d') tensor and
+a (d',d'') tensor:
+    [ (mult t1 t2).[s;s''] =  ∑_{s'≺d'} t1.(s;s') * t2.(s';s'') ]
+*)
 val mult :
   < contr : 'a; cov : 'b > t ->
   < contr : 'b; cov : 'c > t -> < contr : 'a; cov : 'c > t
@@ -94,25 +190,38 @@ val trace :
 val full_contraction :
   < contr : 'a; cov : 'b > t -> < contr : 'b; cov : 'a > t -> float
 
+(** Canonical scalar product of two tensors of dimensions (d, d'):
+[scalar_product t1 t2 = ∑_{s≺d,s'≺d'} t1.[s;s'] * t2.[s;s'] ]  *)
 val scalar_product :
   < contr : 'a; cov : 'b > t -> < contr : 'a; cov : 'b > t -> float
 
+(** [map2 f t1 t2] is the tensor [ t.(s) = f t1.(s) t2.(s) ] *)
 val map2 :
   (float -> float -> float) ->
   < contr : 'a; cov : 'b > t ->
   < contr : 'a; cov : 'b > t -> < contr : 'a; cov : 'b > t
 
+(** [scalar_map f t1] is the tensor [ t.(s) = f t.(s) ] *)
 val scalar_map :
   (float -> float) ->
   < contr : 'a; cov : 'b > t -> < contr : 'a; cov : 'b > t
 
+(** matrix power:
+    [pow_int m 0 = id ]
+    [pow_int m n = m * pow_int m (n-1) ]
+*)
 val pow_int : ('a,'a) matrix -> int -> ('a,'a) matrix
 
+(** Linear algebra basic operations *)
 module Operators: Signatures.tensor_operators with
   type 'a t := 'a t and
   type ('a,'b) matrix := ('a,'b) matrix
+include Signatures.tensor_operators with
+  type 'a t := 'a t and
+  type ('a,'b) matrix := ('a,'b) matrix
 
-val copy : < contr : 'a; cov : 'b > t -> < contr : 'a; cov : 'b > t
+(** {2 Slice related operations}  *)
+
 
 val partial_copy :
   < contr : 'a; cov : 'b > t -> ('a, 'c) Shape.s * ('b, 'd) Shape.s ->
@@ -142,7 +251,3 @@ val set :
 
 val det : < contr : 'a Shape.single; cov : 'a Shape.single > t -> float
 val normal : 'dim vec array -> 'dim vec
-
-include Signatures.tensor_operators with
-  type 'a t := 'a t and
-  type ('a,'b) matrix := ('a,'b) matrix
